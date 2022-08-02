8a5f57dbaedb1031eb63b6124ed7b523b20c232e
Alex Porter <alpo1992@gmail.com.here>
2022-08-01 20:06:22 -0600

    Fixed automobile form

--- ghi/app/src/AutmobileInventoryForm.js
+++ ghi/app/src/AutmobileInventoryForm.js
@@ -7,7 +7,8 @@ class AutomobileInventoryForm extends React.Component {
             color: '',
             year: '',
             vin: '',
-            model_id: '',
+            model: '',
+            models: [],
             
         }
 
@@ -22,6 +23,7 @@ class AutomobileInventoryForm extends React.Component {
     async handleSubmit(event) {
         event.preventDefault();
         const data = {...this.state};
+        delete data.models
         console.log(data);
 
         const automobileUrl = 'http://localhost:8100/api/automobiles/';
@@ -41,7 +43,7 @@ class AutomobileInventoryForm extends React.Component {
             color: '',
             year: '',
             vin: '',
-            model_id: '',
+            model: '',
           };
           this.setState(cleared);
         }
@@ -49,7 +51,7 @@ class AutomobileInventoryForm extends React.Component {
     
     handleModelIdChange(event) {
         const value = event.target.value;
-        this.setState({model_id: value})
+        this.setState({model: value})
     }
 
     handleYearChange(event) {
@@ -65,7 +67,20 @@ class AutomobileInventoryForm extends React.Component {
     handleVinChange(event) {
         const value = event.target.value;
         this.setState({vin: value})
-    }    
+    }
+
+    async componentDidMount() {
+        const url = 'http://localhost:8100/api/models/';
+    
+        const response = await fetch(url);
+    
+        if (response.ok) {
+          const data = await response.json();
+
+          this.setState({models: data.models});
+
+        }
+    }
 
     render () {
         return (
@@ -73,7 +88,7 @@ class AutomobileInventoryForm extends React.Component {
                 <div className="offset-3 col-6">
                     <div className="shadow p-4 mt-4">
                         <h1>Create a new Automobile!</h1>
-                        <form onSubmit={this.handleSubmit} id="create-shoe-form">
+                        <form onSubmit={this.handleSubmit} id="create-automobile-form">
                         <div className="form-floating mb-3">
                             <input onChange={this.handleColorChange} value={this.state.color} placeholder="Color" required type="text" name="color" id="color" className="form-control"/>
                             <label htmlFor="color">Color</label>
@@ -86,9 +101,17 @@ class AutomobileInventoryForm extends React.Component {
                             <input onChange={this.handleVinChange} value={this.state.vin} placeholder="Vin" required type="text" name="vin" id="vin" className="form-control"/>
                             <label htmlFor="vin">Vin</label>
                         </div>
-                        <div className="form-floating mb-3">
-                            <input onChange={this.handleModelIdChange} value={this.state.model_id} placeholder="Model Id" required type="text" name="model_id" id="model_id" className="form-control"/>
-                            <label htmlFor="model_id">Model Id</label>
+                        <div className="mb-3">
+                            <select onChange={this.handleModelIdChange} value={this.state.model} required name="model" id="model" className="form-select">
+                            <option value="">Choose a model</option>
+                            {this.state.models.map(model => {
+                                return (
+                                    <option key={model.id} value={model.id}>
+                                    {model.name}
+                                    </option>
+                                )
+                            })}
+                            </select>
                         </div>
                         <button className="btn btn-primary">Create</button>
                         </form>
