4c5aa0b7b2470248c56927fef48b6c9a91d74284
Alex Porter <alpo1992@gmail.com.here>
2022-08-02 19:33:07 -0600

    added the VIP property to my Service Appointment Model

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -16,6 +16,7 @@ class ServiceAppointment(models.Model):
     scheduled_appointment = models.DateTimeField()
     reason = models.CharField(max_length=200)
     finished = models.BooleanField(default=False)
+    VIP = models.BooleanField(default=False)
     technician = models.ForeignKey(
         "Technician",
         related_name = "appointment",
