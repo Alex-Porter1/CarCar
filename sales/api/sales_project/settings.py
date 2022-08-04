96200a76c514ac733aee7e6277b836b57cbf4588
Alex Porter <alpo1992@gmail.com.here>
2022-08-04 15:29:07 -0600

    fixed errors

--- sales/api/sales_project/settings.py
+++ sales/api/sales_project/settings.py
@@ -9,11 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-<<<<<<< HEAD
-import dj_database_url 
-=======
 import dj_database_url
->>>>>>> main
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
