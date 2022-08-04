86d91d5f6fdc3f61139f319f96aa444921dec844
Alex Porter <alpo1992@gmail.com.here>
2022-08-04 13:43:11 -0600

    fixed minor merge issues

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,10 +1,5 @@
 import { Link } from 'react-router-dom'
 
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-
 function MainPage() {
   return (
     <>
--- inventory/api/inventory_project/settings.py
+++ inventory/api/inventory_project/settings.py
@@ -34,7 +34,6 @@ DEBUG = True
 INSTALLED_APPS = [
     "corsheaders",
     "inventory_rest.apps.InventoryRestConfig",
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
