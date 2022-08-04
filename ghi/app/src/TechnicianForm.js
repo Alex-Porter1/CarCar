d330c949319004dfc512c9b2a646389fdb25b5eb
Alex Porter <alpo1992@gmail.com.here>
2022-08-04 13:56:55 -0600

    Made small adjustments to titles on form pages

--- ghi/app/src/TechnicianForm.js
+++ ghi/app/src/TechnicianForm.js
@@ -55,7 +55,7 @@ class TechnicianForm extends React.Component {
             <div className="row">
                 <div className="offset-3 col-6">
                     <div className="shadow p-4 mt-4">
-                        <h1>Add a technician</h1>
+                        <h1>Add Technician</h1>
                         <form onSubmit={this.handleSubmit} id="create-automobile-form">
                         <div className="form-floating mb-3">
                             <input onChange={this.handleNameChange} value={this.state.technician_name} placeholder="technician_name" required type="text" name="technician_name" id="technician_name" className="form-control"/>
